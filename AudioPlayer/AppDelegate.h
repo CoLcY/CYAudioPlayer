1efcb9228121ebccc85dfe2f54c537dab70284ae
CoLcY <jcareerid@hotmail.com>
2018-05-25 17:13:46 +0800

    1.0

---
+++ AudioPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CYAudioPlayer
+//
+//  Created by CoLcY on 2018/5/25.
+//  Copyright © 2018年 CoLcY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AudioPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CYAudioPlayer
+//
+//  Created by CoLcY on 2018/5/25.
+//  Copyright © 2018年 CoLcY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
